1f513ae819916903ba27411a0ffdc2622eea6ba0
Rafael Mourato <rafaelmourato@MacBook-Air-de-Rafael-2.local>
2025-01-28 22:32:13 -0300

    Operações concluidas referentes ao trabaho

--- server.js
+++ server.js
@@ -1,18 +1,74 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
 
+const prisma = new PrismaClient()
 
 const app = express()
 app.use(express.json())
 
-const tarefas = []
+app.post('/tarefas', async (req,res) => {
 
-app.post('/tarefas', (req,res) => {
-    tarefas.push(req.body)
+    await prisma.tarefa.create({
+        data: {
+            titulo: req.body.titulo,
+            descricao: req.body.descricao,
+            status: req.body.status,
+            dataVencimento: req.body.dataVencimento
+        }
+    })
     res.status(201).json(req.body)
 })
 
-app.get('/tarefas',(req,res) => {
+app.get('/tarefas',async (req,res) => {
+    let tarefas = []
+    if(req.query){
+        tarefas = await prisma.tarefa.findMany({
+            where: {
+                status: req.query.status
+            }
+        })
+    }else{
+        tarefas = await prisma.tarefa.findMany()
+    }
     res.status(200).json(tarefas)
+})
+
+app.get('/tarefas/:id',async (req,res) => {
+
+    const tarefas = await prisma.tarefa.findUnique({
+        where: {
+            id: req.params.id
+        }
+    })
+
+    res.status(200).json(tarefas)
+})
+
+
+app.put('/tarefas/:id', async (req,res) => {
+
+    await prisma.tarefa.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            titulo: req.body.titulo,
+            descricao: req.body.descricao,
+            status: req.body.status,
+            dataVencimento: req.body.dataVencimento
+        }
+    })
+    res.status(201).json(req.body)
+})
+
+app.delete('/tarefas/:id', async(req,res) => {
+
+    await prisma.tarefa.delete({
+        where: {
+            id: req.params.id
+        }
+    })
+    res.status(200).json(req.body)
 })
 
 app.listen(3000)
